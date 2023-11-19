0b55e6c88c3da5fb6d25a4b50dcb35da68f1dd24
fromtheeast710 <theeast710@proton.me>
2023-11-19 21:41:17 +1100

    Setup Skeleton toolkit and deployment

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte'],
+	preprocess: [ vitePreprocess()],
+	
+	vitePlugin: {
+		inspector: true,
+	},
+	kit: {
+		adapter: adapter({
+      pages: 'build/',
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
