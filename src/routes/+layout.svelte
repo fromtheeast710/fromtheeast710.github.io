435bf1b3b29c990d34315ca9facddcc3d8d23bec
fromtheeast710 <theeast710@proton.me>
2023-11-20 13:47:40 +1100

    Add waves

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.postcss';
+	import LayeredWaves from '$lib/assets/layered-waves.svelte';
 </script>
 
-<slot />
+<slot/>
+
+<LayeredWaves class="stack"/>
+
+<style>
+	.stack {
+		aspect-ratio: 960/300;
+		width: 100%;
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: cover;
+	}
+</style>
