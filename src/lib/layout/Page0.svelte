7dc314878a8c73bb4e6f6ff43e8cee0ea951fc05
fromtheeast710 <theeast710@proton.me>
2024-04-09 23:48:50 +1000

    Remove <style> from Page 0

--- src/lib/layout/Page0.svelte
+++ src/lib/layout/Page0.svelte
@@ -10,19 +10,14 @@
 <Page title="Welcome" offsetX=0>
   <ul class="content justify-center justify-items-center align-middle">
     <p>Work still in progress.</p>
-    <p>This is my personal page, a place for me to show off my work.</p>
+    <p>Welcome to my personal page, a place for me to show off my work.</p>
     <p>
       Press <IconArrowBigDownLinesFilled class="icon"/>, <IconArrowBigUpLinesFilled class="icon"/> or click <IconHandClick class="icon"/> to navigate the
-      <i class="gradient-text">waves</i>.
+      <i class="bg-clip-text text-transparent box-decoration-clone
+                bg-gradient-to-br
+                from-primary-300 via-tertiary-500 to-secondary-500
+                hover:drop-shadow-2xl"
+      >waves</i>.
     </p>
   </ul>
 </Page>
-
-<style lang="postcss">
-  .gradient-text {
-    @apply bg-clip-text text-transparent box-decoration-clone
-           bg-gradient-to-br
-           from-primary-300 via-tertiary-500 to-secondary-500
-           hover:drop-shadow-2xl;
-  }
-</style>
