0e6a1ca98d095ba806664d9d9e5b9917425ba963
fromtheeast710 <theeast710@proton.me>
2024-07-27 22:29:25 +1000

    Implement components carousel

--- src/lib/layout/Page2.svelte
+++ src/lib/layout/Page2.svelte
@@ -1,56 +1,37 @@
 <script lang="ts">
   import Page from "./Page.svelte";
 
-  let elemCarousel: HTMLDivElement;
-  const unsplashIds = [ "vjUokUWbFOs", "1aJuPtQJX_I", "Jp6O3FFRdEI", "I3C_eojFVQY", "s0fXOuyTH1M", "z_X0PxmBuIQ" ];
+  import Fractal3D from "./3DFractal.svelte";
+  import IslandOfStability from "./IslandOfStability.svelte";
 
-  function carouselLeft(): void {
-    const x =
-      elemCarousel.scrollLeft === 0
-        ? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
-        : elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
-    elemCarousel.scroll(x, 0);
-  }
+  let id = 1;
+  let carouselEl: HTMLElement;
+  let carouselIds = [
+    {component: Fractal3D, name: "Fractal3D"},
+    {component: IslandOfStability, name: "IslandOfStability"},
+  ];
 
-  function carouselRight(): void {
-    const x =
-      elemCarousel.scrollLeft ===
-      elemCarousel.scrollWidth - elemCarousel.clientWidth
-        ? 0 // loop
-        : elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
-    elemCarousel.scroll(x, 0);
-  }
+  function carouselThumb(index: number) {
+    carouselEl.scroll(carouselEl.clientWidth * index, 0);
 
-  function carouselThumbnail(index: number) {
-    elemCarousel.scroll(elemCarousel.clientWidth * index, 0);
+    id = index;
   }
 </script>
 
-<Page title="Gallery" offsetX="30">
-  <section class="p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center pointer-events-auto">
-    <!-- Full Image -->
-    <div
-      bind:this={elemCarousel}
-      class="snap-center snap-mandatory scroll-smooth flex overflow-x-auto"
-    >
-      {#each unsplashIds as unsplashId}
-        <img
-          class="snap-center w-[1024px] rounded-container-token"
-          src="https://source.unsplash.com/{unsplashId}/1024x768"
-          alt={unsplashId}
-          loading="lazy"
-        />
-      {/each}
-    </div>
+<Page title="Gallery" offsetX=10>
+  <section
+    class="p-3 grid grid-cols-[auto_1fr_auto] gap-0 items-center pointer-events-auto"
+    bind:this={carouselEl}
+  >
+    <svelte:component this={carouselIds[id].component}/>
   </section>
-  <!-- Thumbnails -->
-  <section class="p-4 grid grid-cols-6 gap-4 pointer-events-auto">
-    {#each unsplashIds as unsplashId, i}
-      <button type="button" on:click={() => carouselThumbnail(i)}>
+  <section class="card p-4 grid grid-cols-6 gap-4 pointer-events-auto">
+    {#each carouselIds as id, i}
+      <button type="button" on:click={() => carouselThumb(i)}>
         <img
           class="rounded-container-token"
-          src="https://source.unsplash.com/{unsplashId}/256x256"
-          alt={unsplashId}
+          src="/src/lib/assets/{id.name}.png"
+          alt="{id.name} thumbnail"
           loading="lazy"
         />
       </button>
