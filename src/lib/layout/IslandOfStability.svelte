b61f5e3e02dd816531699cd5a7d73c58c5c52ec5
fromtheeast710 <theeast710@proton.me>
2024-07-27 22:05:41 +1000

    Change <button> to <rect>

--- src/lib/layout/IslandOfStability.svelte
+++ src/lib/layout/IslandOfStability.svelte
@@ -1,116 +1,89 @@
 <script lang="ts">
-  import { computePosition, autoUpdate, offset, shift, flip, arrow
-  } from "@floating-ui/dom";
-  import { popup, storePopup } from "@skeletonlabs/skeleton";
-  import { elements } from "$lib/assets/halflive.json";
+  import { writable } from "svelte/store";
+  import { elements } from "$lib/assets/halflife.json";
   import viridis from "$lib/assets/viridis.json";
 
-  storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
-
   let elCharge = ["H", 1, "Ca", 20, "Zr", 40, "Nd", 60, "Hg", 80, "Fm", 100, "Og", 118];
-  let elNeutron = [177, 150, 120, 90, 60, 30, 1];
-  let xTick = 49.6
-  let yTick = 99.8
+  let elNeutron = [1, 30, 60, 90, 120, 150, 177];
+  let xTick = 450 / 6.01
+  let yTick = 460 / 6.15
+
+  let symbol = writable("H")
+  let mass = writable(1)
+  let proton = writable(1)
+  let neutron = writable(0)
+  let halflife = writable("Stable")
 
   function viridisCol(m: string): string {
     return m == "Stable" ? viridis[0]
-                         : m == "None"
-                         ? ""
                          : Number(m) >= 10e9
                          ? viridis[9]
-                         : Number(m) <= 10e-11
+                         : Number(m) <= 10e-11 || m == "None"
                          ? viridis[255]
                          : viridis[254 - Math.round(251*(Math.log10(Number(m)) + 10)/20)];
   }
 </script>
 
-<!-- TODO: change <button> to <rect> -->
-<ol class="table">
-  {#each elements as el, i}
-    {#if el.state == "m" || el.state == "o" || el.halflife == "None"}
-      <li style="display: none"/>
+<!-- svelte-ignore a11y-no-static-element-interactions a11y-mouse-events-have-key-events -->
+<svg width=500 height=500 class="pointer-events-auto">
+  <g id="table">
+  {#each elements as el}
+    {#if el.state == "m" || el.state == "o"}
+      <rect style="display:none"/>
     {:else}
-      <button
-        type="button"
-        class="element-btn"
-        style="grid-column: {el.charge};
-               grid-row: {178 - (el.mass - el.charge)};
-               background-color: {viridisCol(el.halflife)}"
-        use:popup={{ event: 'hover', target: 'el-' + i, placement: 'right' }}
+      <rect
+        x={(500 / 129) * el.charge + 37}
+        y={(500 / 194.3) * (178 - (el.mass - el.charge))}
+        width=4
+        height=3
+        fill={viridisCol(el.halflife)}
+        on:mouseover={() => {
+          symbol.set(el.symbol),
+          mass.set(el.mass),
+          proton.set(el.charge),
+          neutron.set(el.mass - el.charge)
+          halflife.set(el.halflife)
+        }}
       />
-      <section class="card p-2 shadow-lg variant-filled-secondary" data-popup="el-{i}">
-        <div class="arrow variant-filled-secondary"/>
-        <span>Element:
-          <span class="numbers">
-            <sup>{el.mass}</sup><sub>{el.charge}</sub>
-          </span>
-          {el.symbol}
-          <sub class="numbers">{el.mass - el.charge}</sub>
-        </span>
-        <p>Halflife: {el.halflife}</p>
-      </section>
     {/if}
   {/each}
-</ol>
-
-<svg width=700 height=700>
-  <g>
-    <line x1=55 y1=40 x2=55 y2=640/>
-    <line x1=65 y1=650 x2=660 y2=650/>
-    {#each elNeutron as _, i }
-      <line x1=45 y1={40 + i*yTick} x2=56 y2={40 + i*yTick}/>
-      <text x=40 y={45 + i*yTick} font-size=16
-        text-anchor="end">{elNeutron[i]}</text>
+  </g>
+  <g id="info">
+    <rect x=55 y=10 width=265 height=150 rx=15 ry=15 fill="yellow"/>
+    <text x=185 y=65 text-anchor="middle" font-size=35>{$symbol}</text>
+    <text x=160 y=50 text-anchor="end" font-size=20>{$mass}</text>
+    <text x=160 y=75 text-anchor="end" font-size=20>{$proton}</text>
+    <text x=210 y=75 text-anchor="start" font-size=20>{$neutron}</text>
+    <text x=185 y=115 text-anchor="middle" text-decoration="underline" font-size=22
+      >Halflife (seconds)</text>
+    <text x=185 y=140 text-anchor="middle" font-size=18>{$halflife}</text>
+  </g>
+  <g id="scale">
+    <line x1=35 y1=9 x2=35 y2=460/>
+    <line x1=39 y1=470 x2=490 y2=470/>
+    {#each elNeutron.reverse() as _, i}
+      <line x1=30 y1={10 + i*yTick} x2=35 y2={10 + i*yTick}/>
+      <text x=26 y={15 + i*yTick} font-size=16 text-anchor="end" font-style="italic"
+        >{elNeutron[i]}</text>
     {/each}
-    {#each elCharge as _, i }
-      {#if typeof(elCharge[i]) == "string"}
-        <line x1={65 + i*xTick} y1=649 x2={65 + i*xTick} y2=660/>
-        <text x={60 + i*xTick} y=683 font-size=16>{elCharge[i]}</text>
-        <text x={58 + i*xTick} y=675 font-size=12 font-style="italic"
-          text-anchor="end">{elCharge[i+1]}</text>
-      {/if}
+    {#each elCharge.filter((c) => typeof c == "string") as _, i}
+      <line x1={40 + i*xTick} y1=470 x2={40 + i*xTick} y2=475/>
+      <text x={32 + i*xTick} y=495 font-size=16>{elCharge[i*2]}</text>
+      <text x={29 + i*xTick} y=499 font-size=12 text-anchor="end" font-style="italic"
+        >{elCharge[i*2+1]}</text>
     {/each}
   </g>
 </svg>
 
 <style lang="postcss">
-  .table {
-    width: 700px;
-    height: 700px;
-    position: absolute;
-    display: grid;
-    pointer-events: auto;
-    padding: 42px;
-    padding-left: 67px;
-    padding-bottom: 62px;
-    background-color: transparent;
-  }
-  .numbers {
-    flex-direction: column;
-  }
-  text {
-    fill: gray;
-  }
   line {
     stroke: gray;
     stroke-width: 2;
   }
-  p {
-    font-size: 1rem;
-    line-height: 1.5rem;
-    padding-bottom: 0px;
-    white-space: nowrap;
-    overflow: hidden;
+  text {
+    fill: gray;
   }
-  span {
-    display: flex;
-    align-items: center;
-  }
-  sup, sub {
-    padding: 0.1rem;
-  }
-  [data-popup] {
-    transition-property: none;
-	  transition-duration: 0s
+  #info>text {
+    fill: black;
   }
 </style>
