00183101749c1eb97cf8e4bc6904acecfc432020
fromtheeast710 <theeast710@proton.me>
2024-09-29 02:10:47 +1000

    Change to correct icon and reformat Page4

--- src/lib/layout/Page4.svelte
+++ src/lib/layout/Page4.svelte
@@ -1,31 +1,57 @@
 <script>
   import {
-    IconBrandGithub, IconBrandLinkedin, IconBrandDiscord, IconBrandMatrix, IconBrandLeetcode
-    // IconBrandHackerrank
+    IconBrandGithub,
+    IconBrandLinkedin,
+    IconBrandDiscord,
+    IconBrandMatrix,
+    IconBrandHackerrank,
   } from "@tabler/icons-svelte";
   import Page from "./Page.svelte";
 </script>
 
-<Page title="Contacts & Resume" offsetX=35>
-  <p class="pt-[50px]">
-  Check out my other works and contacts at:
-  </p>
+<Page title="Contacts & Resume" offsetX="35">
+  <p class="pt-[50px]">Check out my other works and contacts at:</p>
   <section class="flex p-10 grid-cols-4">
-    <button type="button" class="btn-icon variant-ghost"
-      on:click={() => location.href = "https://github.com/fromtheeast710"}
-    ><IconBrandGithub/></button>
-    <button type="button" class="btn-icon variant-ghost"
-      on:click={() => location.href = "https://www.linkedin.com/in/đông-phương-phạm-a65192270%2f"}
-    ><IconBrandLinkedin/></button>
-    <button type="button" class="btn-icon variant-ghost"
-      on:click={() => location.href = "https://www.discordapp.com/users/yeast710"}
-    ><IconBrandDiscord/></button>
-    <button type="button" class="btn-icon variant-ghost"
-      on:click={() => location.href = "https://matrix.to/#/@fromtheeast:matrix.org"}
-    ><IconBrandMatrix/></button>
-    <button type="button" class="btn-icon variant-ghost"
-      on:click={() => location.href = "https://www.hackerrank.com/profile/fromtheeast710"}
-    ><IconBrandLeetcode/></button>
+    <button
+      type="button"
+      class="btn-icon variant-ghost"
+      on:click={() => (location.href = "https://github.com/fromtheeast710")}
+    >
+      <IconBrandGithub />
+    </button>
+    <button
+      type="button"
+      class="btn-icon variant-ghost"
+      on:click={() =>
+        (location.href =
+          "https://www.linkedin.com/in/đông-phương-phạm-a65192270%2f")}
+    >
+      <IconBrandLinkedin />
+    </button>
+    <button
+      type="button"
+      class="btn-icon variant-ghost"
+      on:click={() =>
+        (location.href = "https://www.discordapp.com/users/yeast710")}
+    >
+      <IconBrandDiscord />
+    </button>
+    <button
+      type="button"
+      class="btn-icon variant-ghost"
+      on:click={() =>
+        (location.href = "https://matrix.to/#/@fromtheeast:matrix.org")}
+    >
+      <IconBrandMatrix />
+    </button>
+    <button
+      type="button"
+      class="btn-icon variant-ghost"
+      on:click={() =>
+        (location.href = "https://www.hackerrank.com/profile/fromtheeast710")}
+    >
+      <IconBrandHackerrank />
+    </button>
   </section>
 
   <!-- <p>And here's my
@@ -40,7 +66,7 @@
     pointer-events: all;
     align-items: center;
     align-self: center;
-    margin:auto;
+    margin: auto;
   }
   a:hover#svelte {
     filter: drop-shadow(0 0 2em #e26c1eaa);
