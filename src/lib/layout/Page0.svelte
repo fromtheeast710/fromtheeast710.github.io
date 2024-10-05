f2523325586826eefd806312697d1496945055c0
fromtheeast710 <theeast710@proton.me>
2024-10-06 01:18:34 +1000

    Update intro

--- src/lib/layout/Page0.svelte
+++ src/lib/layout/Page0.svelte
@@ -2,31 +2,44 @@
   import {
     IconArrowBigDownLinesFilled,
     IconArrowBigUpLinesFilled,
-    IconHandClick
+    IconHandClick,
   } from "@tabler/icons-svelte";
   import Page from "./Page.svelte";
 </script>
 
-<Page title="Welcome" offsetX=0>
+<Page title="Welcome" offsetX="0">
   <ul class="content justify-center justify-items-center align-middle">
     <p>Work still in progress.</p>
 
     <p>
-      Welcome to my personal page! My name is Phạm Đông Phương, nicknamed <i>East</i>. I am a second-year Computer
-      Science student at Western Sydney University in <b>Sydney, Australia</b> 🇦🇺.
+      Welcome to my personal page! My name is Phạm Đông Phương, nicknamed <i
+        >East</i
+      >. I am a second-year Computer Science student at Western Sydney
+      University in <b>Sydney, Australia</b> 🇦🇺.
     </p>
 
-    <p>I have a passion for learning about technology, programming and mathematics.</p>
-
-    <p>I am currently focusing on <b>programminng in <i class="text-orange-500">Rust</i></b> through my projects.</p>
+    <p>
+      I am a quick learner and have a passion for learning about computer
+      science, programming languages, electronics and mathematics.
+    </p>
 
     <p>
-      Press <IconArrowBigDownLinesFilled class="icon kbd"/>, <IconArrowBigUpLinesFilled class="icon kbd"/> or click
-      <IconHandClick class="icon kbd"/> to navigate the
-      <i class="bg-clip-text text-transparent box-decoration-clone bg-gradient-to-br
+      I am currently focusing on <b
+        >programminng in <i class="text-orange-500">Rust</i></b
+      >
+      and <b>the ROS robotics stack</b> through my projects.
+    </p>
+
+    <p>
+      Press <IconArrowBigDownLinesFilled class="icon kbd" />, <IconArrowBigUpLinesFilled
+        class="icon kbd"
+      /> or click
+      <IconHandClick class="icon kbd" /> to navigate the
+      <i
+        class="bg-clip-text text-transparent box-decoration-clone bg-gradient-to-br
                 from-primary-300 via-tertiary-500 to-secondary-500
-                hover:drop-shadow-2xl"
-      >waves</i>.
+                hover:drop-shadow-2xl">waves</i
+      >.
     </p>
   </ul>
 </Page>
--- src/lib/layout/Page1.svelte
+++ src/lib/layout/Page1.svelte
@@ -4,9 +4,9 @@
 
 <Page title="Skills" offsetX="5">
   <p>
-    I have epxeriences in imperative programming with the following languages:
+    I have epxeriences in designing software with the following languages:
     <span id="c-lang">C/C++</span>, <span id="py-lang">Python</span>,
-    <span id="rs-lang">Rust</span> and the web stack
+    <span id="rs-lang">Rust</span> along with the web stack
     <span id="html-lang">HTML</span>/<span id="css-lang">CSS</span>/<span
       id="ts-lang">TS</span
     >
@@ -14,59 +14,25 @@
   </p>
 
   <p>
-    I am also interested in functional programming with <span id="nix-lang"
-      >Nix</span
-    >
-    and is planning to learn
-    <span id="ml-lang">OCaml</span>.
-    <!-- I also occasionally play with electronics
-    and want to learn <span id="v-lang">SystemVerilog</span> for FPGAs design. -->
+    In my free time, I experiment on functional programming with <span
+      id="nix-lang">Nix</span
+    >, <span id="ml-lang">OCaml</span> and is planning to learn
+    <span id="v-lang">SystemVerilog</span>
+    for FPGAs design.
   </p>
 
   <p>
-    I am familiar with Git for version control along with VSCodium and Artix
-    Linux™ for my development environment.
+    I extensively utilize open-source tools and libraries such as Git, Linux,
+    Docker, etc for my projects. I'm currently working as an open-source
+    developer and contributer.
   </p>
 
-  <p>
-    I extensively use open-source libraries in my projects and is currently
-    working as an open-source developer and contributor.
-  </p>
+  <p>Other than dev work, I enjoy playing ping-pong and meeting new people.</p>
 </Page>
 
 <style>
   span {
     font-weight: bold;
     font-style: italic;
-  }
-  #c-lang {
-    color: hsl(208 100% 29%);
-  }
-  #rs-lang {
-    color: hsl(18 100% 48%);
-  }
-  #nix-lang {
-    color: hsl(239 47% 53%);
-  }
-  #html-lang {
-    color: hsl(18 88% 56%);
-  }
-  #css-lang {
-    color: hsl(223 86% 55%);
-  }
-  #ts-lang {
-    color: hsl(211 60% 49%);
-  }
-  #py-lang {
-    color: hsl(47 100% 70%);
-  }
-  #sv-lang {
-    color: hsl(15 100% 53%);
-  }
-  #ml-lang {
-    color: hsl(27 90% 49%);
-  }
-  #v-lang {
-    color: hsl(0 0% 29%);
   }
 </style>
